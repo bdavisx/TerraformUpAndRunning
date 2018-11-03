dc3d5e7187b6d726ea95616706a8e39eb2f57aee
bill <bdavisx@yahoo.com>
2018-11-02 20:58:15 -0500

    added load balancing

--- step1/main.tf
+++ step1/main.tf
@@ -15,21 +15,87 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-40d28157"
+resource "aws_launch_configuration" "bd-example-launch-configuration" {
+  image_id = "ami-40d28157"
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
-    echo "Hello, World" > index.html
+    echo "<h1>Galvin, don't f@ck with me!</h1>" > index.html
     nohup busybox httpd -f -p "${var.server_port}" &
     EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "bd-example-autoscaling-group" {
+  launch_configuration = "${aws_launch_configuration.bd-example-launch-configuration.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  load_balancers    = ["${aws_elb.bd-example-elastic-load-balancer.name}"]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "bd-elb-example-security-group" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "bd-example-elastic-load-balancer" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.bd-elb-example-security-group.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.bd-example-elastic-load-balancer.dns_name}"
 }
